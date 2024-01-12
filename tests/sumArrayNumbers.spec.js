c87d0eb24150cf93f8cbe8d18d18b5d621403bec
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-12 17:03:29 +0100

    lab done

--- tests/sumArrayNumbers.spec.js
+++ tests/sumArrayNumbers.spec.js
@@ -1,14 +1,28 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 5", () => {
-    describe("Function - sumArrayNumbers", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+  describe("Function - sumArrayNumbers", () => {
+    it("should be defined", () => {
+      expect(sumArrayNumbers).toBeDefined();
+    });
 
-    })    
-})
+    it("should take one argument of type array. The array should contain numbers.", () => {
+      expect(sumArrayNumbers).toHaveLength(1);
+      expect(Array.isArray(sumArrayNumbers([1, 2, 3]))).toBe(true);
+    });
 
-*/
+    it("should return the sum of all numbers in the array", () => {
+      expect(sumArrayNumbers([1, 2, 3])).toEqual(6);
+      expect(sumArrayNumbers([1, 1.5, 100])).toEqual(102.5);
+      expect(sumArrayNumbers([0, 1])).toEqual(1);
+    });
+
+    it("should return undefined if the argument passed is not an array", () => {
+      expect(sumArrayNumbers(42)).toBe(undefined);
+    });
+
+    it("should return 0 if the argument passed is an empty array", () => {
+      expect(sumArrayNumbers([])).toEqual(0);
+    });
+  });
+});
+
+//I GIVE UP//
