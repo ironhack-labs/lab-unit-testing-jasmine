83f9d168cc255292c48b3050ace7f1851aed89b5
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-12 11:50:31 +0100

    iteration 3

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,12 @@
-function calculateArea() {
+function calculateArea(a, b) {
+
+  if (
+    typeof a !== "number" 
+    || typeof b !== "number"
+    || typeof a   === "undefined"
+    || typeof b === "undefined"
+  ) return undefined  
+
+  return a * b
 
 }
