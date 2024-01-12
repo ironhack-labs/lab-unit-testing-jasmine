6124f999ad199f41bc5882e046e148e5d1ca73c6
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-12 11:39:20 +0100

    iteration 2

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,21 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+  describe("Function - divide", () => {
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
 
-    })    
-})
+    it("should return the division of the two numbers", () => {
+      expect(divide(4, 2)).toEqual(2);
+    });
 
-*/
+    it("should take two numbers as an argument", () => {
+      expect(divide.length).toBe(2);
+    });
+
+    it("should return undefined if no arguments are provided", () => {
+      expect(divide()).toEqual(undefined);
+    });
+  });
+});
