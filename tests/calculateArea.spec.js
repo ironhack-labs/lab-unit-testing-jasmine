04f2520f6bdce6d04f7fbae76a583b7e01b87d5a
Juan I. Solis <solid.dogtraining@gmail.com>
2024-01-13 19:23:10 +0100

    solved lab

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,35 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+    describe("Function calculateArea", () => {
+        it("should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
 
-    })    
-})
+        it("should take two numbers as arguments", () => {
+expect( calculateArea.length).toBe(2);
+        });
 
-*/
+        it("should return a number representing the area of a rectangle", () => {
+            expect( calculateArea(1,2) ).toEqual(2);
+            expect( calculateArea(3,4) ).toEqual(12);
+            expect( calculateArea(5,5)).toEqual(25);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect( calculateArea(1) ).toEqual(undefined);
+            expect( calculateArea() ).toEqual(undefined);
+            expect( calculateArea(undefined, 1) ).toEqual(undefined);
+        });
+
+        it("should return undefined if any of the arguments is not a number", () => {
+            expect( calculateArea("1",2) ).toEqual(undefined);
+            expect( calculateArea(3,"4") ).toEqual(undefined);
+            expect( calculateArea("5","6") ).toEqual(undefined);
+        });
+
+    });    
+});
+    
--- tests/sumArrayNumbers.spec.js
+++ tests/sumArrayNumbers.spec.js
@@ -1,14 +1,32 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 5", () => {
-    describe("Function - sumArrayNumbers", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+    describe("Function sumArrayNumbers", () => {
+        it("should be defined", () => {
+            expect(sumArrayNumbers).toBeDefined();
+        });
 
-    })    
-})
+        it("should take one argument", () =>{
+expect(sumArrayNumbers.length).toBe(1);
+        });
 
-*/
+        it("should return the sum of all numbers in the array", () => {
+            expect(sumArrayNumbers([1, 2, 3])).toEqual(6);
+            expect(sumArrayNumbers([3, 4, 5])).toEqual(12);
+            expect(sumArrayNumbers([100, 47])).toEqual(147);
+        });
+
+        it("should return undefined if the argument passed is not an array", () => {
+            expect(sumArrayNumbers(123)).toEqual(undefined);
+            expect(sumArrayNumbers("1")).toEqual(undefined);
+            expect(sumArrayNumbers({})).toEqual(undefined);
+        });
+
+        it("should return 0 if the argument passed is an empty array", () => {
+            expect(sumArrayNumbers([])).toEqual(0);
+        });
+
+    });    
+});
