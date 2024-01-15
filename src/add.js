43845dfaa63ecb532e593e8b6dd8f550d33abbc4
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-01-15 09:52:07 +0100

    finish first iteration

--- src/add.js
+++ src/add.js
@@ -1,7 +1,11 @@
-function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
-        return undefined;
-    }
-
-    return numOne + numTwo;
-}
+function add(numOne, numTwo) {
+  if (
+    numOne === undefined ||
+    numTwo === undefined ||
+    typeof numOne !== "number" ||
+    typeof numTwo !== "number"
+  ) {
+    return undefined;
+  }
+  return numOne + numTwo;
+};
