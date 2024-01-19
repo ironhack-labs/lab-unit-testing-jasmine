023d5d1f0cc475817c2ff4a8f73fefdd5a27da9a
Pauline Cvt <paulinecvt2021@gmail.com>
2024-01-19 16:01:48 +0100

    first and second ite

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,20 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
 
 describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
+    describe("Function divide", () => {
+        it("The function should be defined.", () => {
+            expect(divide).toBeDefined();
+        });
+        it("The function should take two numbers as arguments.", () => {
+            expect(divide('abc', 5)).toBeUndefined();
+        });
+        it("The function should return the division of the two numbers.", () => {
+            expect(divide(10,2)).toEqual(5);
+            expect(divide(20, 10)).toEqual(2);
+        }) ;
+        it("The function should return undefined if any of the arguments is not provided", () => {
+            expect(divide(undefined)).toEqual(undefined);
+        });     
+    });
 })
-
-*/
