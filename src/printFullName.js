41f5661990b2417a362b8c88816d0d293c8eccf9
JuanHecht <112518908+JuanHecht@users.noreply.github.com>
2024-01-19 16:52:19 +0000

    [WDFTLIS012024] Juan Hecht

--- src/printFullName.js
+++ src/printFullName.js
@@ -1,3 +1,11 @@
-function printFullName() {
+function printFullName(names) {
+    if (typeof names !== "object")return undefined;
+        
+    // if (typeof names.firstName != "string" || typeof names.lastName != "string ") return undefined;
     
+    return names.firstName + " " + names.lastName;
 }
+
+// Write an object with the properties firstName: John and lastName: Doe
+let person = {firstName:"John",lastName:"Doe"};
+console.log(printFullName(person))
--- tests/printFullName.spec.js
+++ tests/printFullName.spec.js
@@ -1,14 +1,24 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 4", () => {
     describe("Function - printFullName", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("The function should be defined", ()=>{
+            expect(printFullName).toBeDefined();
+        });
+        it("should take one argument", ()=>{
+            expect(printFullName.length).toEqual(1);
+        });
+        it("should return the full name of a person when provided with first and last names", ()=>{
+            expect(printFullName({ firstName: "Rick"}, {lastName: "Ed"})).toEqual("Rick Ed");
+        });
+        it("In case the argument passed is not an object, the function should return undefined", ()=>{
+            expect(printFullName('Jane')).toBeUndefined();
+            expect(printFullName(76)).toBeUndefined();
+            expect(printFullName(true)).toBeUndefined();
+        });
 
     })    
 })
 
-*/
