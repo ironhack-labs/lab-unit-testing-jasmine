39f72a33b424ee0c6f852e03c260a49866287ee3
Joao Dias <dias.joao1983@gmail.com>
2024-01-19 21:39:06 +0000

    solved

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,24 +1,32 @@
 describe("Iteration 1 | Getting Started", () => {
-    describe("Function add", () => {
-      it("should be defined", () => {
-        expect(add).toBeDefined();
-      });
-  
-      it("should take two arguments", () => {
-        expect(add.length).toBe(2);
-      });
-  
-      it("should return the sum of the two numbers", () => {
-        expect(add(1, 2)).toEqual(3);
-        expect(add(3, 4)).toEqual(7);
-        expect(add(100, 47)).toEqual(147);
-      });
-  
-      it("should return undefined if any of the arguments is not provided", () => {
-        expect(add(1)).toEqual(undefined);
-        expect(add()).toEqual(undefined);
-        expect(add(undefined, 1)).toEqual(undefined);
-      });
+  describe("Function add", () => {
+    it("should be defined", () => {
+      expect(add).toBeDefined();
+    });
+
+    it("should take two arguments", () => {
+      expect(add.length).toBe(2);
+    });
+
+    it("should return the sum of the two numbers", () => {
+      expect(add(1, 2)).toEqual(3);
+      expect(add(3, 4)).toEqual(7);
+      expect(add(100, 47)).toEqual(147);
+    });
+
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(add(1)).toEqual(undefined);
+      expect(add()).toEqual(undefined);
+      expect(add(undefined, 1)).toEqual(undefined);
+    });
+
+    // TEST - Iteration 1.6
+    it("should return undefined if any of the arguments is not a number", () => {
+      expect(add(1, "2")).toEqual(undefined);
+      expect(add("3", 4)).toEqual(undefined);
+      expect(add("100", "47")).toEqual(undefined);
     });
   });
+});
+
   
