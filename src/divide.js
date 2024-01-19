f51bb957921ded7a2dc9af4b54791cc717489dbd
TiagoSilvina <tiagoafsilvina@gmail.com>
2024-01-19 16:12:23 +0000

    jasmine testing lab completed

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,8 @@
-function divide () {
+function divide (num1,num2) {
+    if (typeof num1 != "number" || typeof num2 != "number"){
+        return undefined;
+    }
+   
+    return num1/num2;
        
 }
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,33 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+        });
+        it("should accept two parameters", ()=>{
+            expect(divide.length).toBe(2);
+        })
+        it("should return the division of the two numbers", ()=>{
+            expect(divide(2,2)).toEqual(1);
+            expect(divide(10,2)).toEqual(5);
+            expect(divide(100,2)).toEqual(50);
+        })
+        it("should return undefined if any of the arguments is not provided", ()=>{
+            expect(divide(1)).toEqual(undefined);
+            expect(divide()).toEqual(undefined);
+            expect(divide(undefined,1)).toEqual(undefined);
+        })
+        it("should return undefined if any of the arguments is not a number", ()=>{
+            expect(divide(1,"2")).toEqual(undefined);
+            expect(divide("3,",4)).toEqual(undefined);
+            expect(divide("5","6")).toEqual(undefined);
+        })
 
     })    
 })
 
-*/
