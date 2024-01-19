783063ee71213055a80447a7376dbca28d9594ea
Egbagba <egbagba02@gmail.com>
2024-01-19 18:21:11 +0000

    Test-Jas-Lab-Done

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,10 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it("The function should return undefined if any of the two arguments is not a number", ()=>{
+        expect(add(1, 6)).toEqual(7);
+      })
     });
   });
   
