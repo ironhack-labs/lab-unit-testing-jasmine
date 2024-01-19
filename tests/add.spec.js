67d02014e9284ab909b0fbd41faca61012320408
pepolopes86 <pedro_lopes86@hotmail.com>
2024-01-19 16:19:24 +0000

    lab solved

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -9,15 +9,21 @@ describe("Iteration 1 | Getting Started", () => {
       });
   
       it("should return the sum of the two numbers", () => {
-        expect(add(1, 2)).toEqual(3);
-        expect(add(3, 4)).toEqual(7);
-        expect(add(100, 47)).toEqual(147);
+        expect(add(2, 2)).toEqual(4);
+        expect(add(5, 5)).toEqual(10);
+        expect(add(20, 20)).toEqual(40);
       });
   
       it("should return undefined if any of the arguments is not provided", () => {
         expect(add(1)).toEqual(undefined);
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
+      });
+
+      it("should return undefined if any of the two arguments is not a number.", () => {
+        expect(add(1,'2')).toEqual(undefined);
+        expect(add('3', 4)).toEqual(undefined);
+        expect(add('100', '47')).toEqual(undefined);
       });
     });
   });
