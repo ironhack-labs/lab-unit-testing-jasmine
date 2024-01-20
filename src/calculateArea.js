4bc33e6135db0553a86fdf0c93304089be99d7f3
Aron <emmanuel.aron@gmail.com>
2024-01-20 04:23:42 +0100

    Solved lab

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,7 @@
-function calculateArea() {
 
+function calculateArea(a , b) {
+    if(a === undefined || b === undefined){
+        return undefined
+    }
+    return a * b
 }
--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,10 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it("should return undefined if any of the two arguments is not a number", () => {
+        expect(add(1, "hello")).toEqual(undefined);
+       // expect(add("what", 4)).toEqual(undefined);
+      });
     });
   });
   
