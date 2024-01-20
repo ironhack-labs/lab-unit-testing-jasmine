f8d27d6fb0341bdce615601cf5ee9d6de13ed773
Daniel Amaral <dfbamaral@gmail.com>
2024-01-20 18:58:16 +0000

    lab solved

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -12,9 +12,15 @@ describe("Iteration 2", () => {
        expect(divide.length).toBe(2); 
     }) 
     it ("The function should return the division of the two numbers",() =>{
+        expect(divide(4, 2)).toEqual(2);
+        expect(divide(15, 5)).toEqual(3);
+        expect(divide(120, 30)).toEqual(4);
         
     }) 
     it ("The function should return undefined if any of the arguments is not provided",() =>{
+        expect(divide(4)).toEqual(undefined);
+        expect(divide()).toEqual(undefined);
+        expect(add(undefined, 5)).toEqual(undefined);
         
     }) 
   
