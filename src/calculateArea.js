0907e3e00a9628e353f5ceed17c65707d491a050
Diogo Coelho <diogo_coelho16@hotmail.com>
2024-01-21 22:00:49 +0000

    lab-unit-testing-jasmine

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,7 @@
-function calculateArea() {
-
+function calculateArea(num1, num2) {
+    if (num1 === "" || num2 === "") {
+        return undefined
+    }
+    
+    return num1 * num2
 }
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,21 @@
-// UNCOMMENT THE CODE BELOW TO START
+describe("Function calculateArea", () => {
+  it("should be defined", () => {
+    expect(calculateArea).toBeDefined();
+  });
 
-/*
+  it("should take two numbers as a argument", () => {
+    expect(calculateArea.length).toBe(2);
+  });
 
-describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+  it("should return a number representing the area of a rectangle, ", () => {
+    expect(calculateArea(1, 2)).toEqual(2);
+    expect(calculateArea(3, 4)).toEqual(12);
+    expect(calculateArea(100, 47)).toEqual(4700);
+  });
 
-    })    
-})
-
-*/
+  it("should return undefined if any of the arguments is not provided" , () => {
+    expect(calculateArea(1 , "")).toEqual(undefined);
+    expect(calculateArea("", 1)).toEqual(undefined);
+    expect(calculateArea("" , "")).toEqual(undefined);
+});
+});
