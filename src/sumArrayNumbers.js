81e76a2b2434462c67d4003243a28deaf91499b3
MateusCTO <farias.mateus@gmail.com>
2024-01-21 05:47:14 +0000

    Lab done

--- src/sumArrayNumbers.js
+++ src/sumArrayNumbers.js
@@ -1,3 +1,11 @@
-function sumArrayNumbers() {
+function sumArrayNumbers(array) {
+  if (!Array.isArray(array)) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
 
-}
+  return sum;
+};
