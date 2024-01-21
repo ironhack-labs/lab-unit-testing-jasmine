f5e370d59d0134d8629f066ae37c3385fe22d82f
Gabriela Silva <gvsde8@gmail.com>
2024-01-21 13:57:16 +0000

    complete

--- src/printFullName.js
+++ src/printFullName.js
@@ -1,3 +1,14 @@
-function printFullName() {
-    
+function printFullName(fullName) {
+    fullName = {
+		firstName: fullName.firstName,
+		lastName: fullName.lastName
+	}
+	
+	if (`${fullName.firstName} ${fullName.lastName} ` !== "Gabriela Silva") {
+        return undefined;
+    }
+
+	let result = `${fullName.firstName}  ${fullName.lastName} `;
+
+	return result;
 }
--- tests/sumArrayNumbers.spec.js
+++ tests/sumArrayNumbers.spec.js
@@ -1,14 +1,34 @@
 // UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 5", () => {
     describe("Function - sumArrayNumbers", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+       it("should be defined", () => {
+            expect(sumArrayNumbers).toBeDefined();
+          });
+      
+          it("should take one argument", () => {
+            expect(sumArrayNumbers.length).toBe(1);
+          });
 
+          it("should return an array of numbers", () => {
+            expect(sumArrayNumbers([])).toEqual([]);
+            expect(sumArrayNumbers([1, 4, 9, 7])).toEqual([1, 4, 9, 7]);
+          });
+
+          it("should return a sum of all numbers in the array", () => {
+            expect(sumArrayNumbers([1, 4, 9, 7])).toEqual(21);
+            expect(sumArrayNumbers([1, 4, 6])).toEqual(11);
+          });
+
+          it("should return undefined if the argument is not an array", () => {
+            expect(sumArrayNumbers(1)).toEqual(undefined);
+            expect(sumArrayNumbers()).toEqual(undefined);
+            expect(sumArrayNumbers("")).toEqual(undefined);
+            expect(sumArrayNumbers({})).toEqual(undefined);
+            expect(sumArrayNumbers(true)).toEqual(undefined);
+          });
+
+          it("should return 0 if the argument is an empty array", () => {
+            expect(sumArrayNumbers([])).toEqual(0);
+          });
     })    
 })
-
-*/
