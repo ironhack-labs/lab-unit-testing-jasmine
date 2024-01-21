22be4fd5923eb4e36fc2b5b854dbef1ae63506d4
Laia Navalon <laianavalon@gmail.com>
2024-01-21 18:59:33 +0100

    Solved lab

--- src/sumArrayNumbers.js
+++ src/sumArrayNumbers.js
@@ -1,3 +1,19 @@
-function sumArrayNumbers() {
+function sumArrayNumbers(arr) {
+    if (!Array.isArray(arr)) {
+        return undefined;
+    }
+    else if (arr.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    
+    arr.forEach(function(element) {
+        sum += element;
+    })
 
+    return sum;
 }
+
+const num = [4, 5, 6, 7]
+
+sumArrayNumbers(num)
