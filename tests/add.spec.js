01567060f6955794c95d557857750ebc493ffa22
„Aristidis <ari86@MacBook-Pro.local>
2024-01-21 18:13:29 +0100

    Solved lab

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,24 +1,29 @@
 describe("Iteration 1 | Getting Started", () => {
-    describe("Function add", () => {
-      it("should be defined", () => {
-        expect(add).toBeDefined();
-      });
-  
-      it("should take two arguments", () => {
-        expect(add.length).toBe(2);
-      });
-  
-      it("should return the sum of the two numbers", () => {
-        expect(add(1, 2)).toEqual(3);
-        expect(add(3, 4)).toEqual(7);
-        expect(add(100, 47)).toEqual(147);
-      });
-  
-      it("should return undefined if any of the arguments is not provided", () => {
-        expect(add(1)).toEqual(undefined);
-        expect(add()).toEqual(undefined);
-        expect(add(undefined, 1)).toEqual(undefined);
-      });
+  describe("Function add", () => {
+    it("should be defined", () => {
+      expect(add).toBeDefined();
+    });
+
+    it("should take two arguments", () => {
+      expect(add.length).toBe(2);
+    });
+
+    it("should return the sum of the two numbers", () => {
+      expect(add(1, 2)).toEqual(3);
+      expect(add(3, 4)).toEqual(7);
+      expect(add(100, 47)).toEqual(147);
+    });
+
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(add(1)).toEqual(undefined);
+      expect(add()).toEqual(undefined);
+      expect(add(undefined, 1)).toEqual(undefined);
+    });
+
+    it("should return undefiened, if any of the two arguments is not a number", () => {
+      expect(add("not a number", 5)).toBeUndefined();
+      expect(add(10, "not a number")).toBeUndefined();
+      expect(add("not a number", "still not a number")).toBeUndefined();
     });
   });
-  
+});
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,24 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+  describe("Function - divide", () => {
+    // Each `it` block represents 1 test. You can use the following as a template:
+    it("should be defined", () => {
+      expect(divide).toBeDefined();
+    });
+    it("should take two numbers as arguments", () => {
+      expect(divide.length).toBe(2);
+      expect(divide).toBe("numbers");
+    });
+    it("should return the division of the two numbers", () => {
+      expect(divide(6, 2)).toBe(3);
+      expect(divide(10, 2)).toBe(5);
+      expect(divide(6, 0)).toBe(undefined);
+    });
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(divide(1)).toEqual(undefined);
+      expect(divide()).toEqual(undefined);
+      expect(divide(undefined, 1)).toEqual(undefined);
+    });
+  });
+});
