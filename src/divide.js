b02527dc59e5bd751f20d7b98ffc433129730077
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-02-12 12:09:10 -0500

    lab done

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,13 @@
-function divide () {
+function divide (num1, num2) {
+
+    if (num1 === undefined || num2 === undefined ) {
+        return undefined
+    }
        
+    return num1 / num2
 }
+
+// The function should be defined.
+// The function should take two numbers as arguments.
+// The function should return the division of the two numbers.
+// The function should return undefined if any of the arguments is not provided
