615581dd79e9d9433481ab4f8726e88391fadf1e
joserobles <jarobles1018@gmail.com>
2024-02-12 13:26:39 -0400

    Lab Done

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,9 @@
-function calculateArea() {
-
-}
+function calculateArea(width, length) {
+    if (typeof width !== "number" || typeof length !== "number") {
+      return undefined;
+    }
+  
+    return width * length;
+  }
+  
+  
