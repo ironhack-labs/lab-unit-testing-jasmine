25da38db4b28bf66cb78726432a43d1cb1db6813
Dalton <daltongetbaker@gmail.com>
2024-02-12 12:29:13 -0500

    lab done 2

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,8 @@
-function divide () {
-       
+function divide (a, b) {
+      if(typeof a !== "number" || typeof b !== "number") {
+        return undefined;
+      }
+        
+      return a / b
 }
+
