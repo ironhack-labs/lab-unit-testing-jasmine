796dabbb6fc9734579bbd4d70724a48f108d51bb
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-16 16:29:14 +0100

    done minus bonuses

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,20 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+  describe("Function - calculateArea", () => {
+    it("should be defined", () => {
+      expect(calculateArea).toBeDefined();
+    });
 
-    })    
-})
+    it("should return the area of a rectangle", () => {
+      expect(calculateArea(4, 5)).toBe(20);
+      expect(calculateArea(10, 2)).toBe(20);
+      expect(calculateArea(3, 7)).toBe(21);
+    });
 
-*/
+    it("should return undefined if any argument is not provided", () => {
+      expect(calculateArea(4)).toBeUndefined();
+      expect(calculateArea()).toBeUndefined();
+    });
+  });
+});
