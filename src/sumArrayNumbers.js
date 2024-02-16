81f366bc12ceca883be4146795236ad72fe50768
shaza kashlan <eng.shazakashlan@gmail.com>
2024-02-16 17:57:14 +0100

    done with Iteration 4,5

--- src/sumArrayNumbers.js
+++ src/sumArrayNumbers.js
@@ -1,3 +1,9 @@
-function sumArrayNumbers() {
-
+function sumArrayNumbers(array) {
+  if (!Array.isArray(array)) {
+    return undefined;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((acc, curr) => acc + curr, 0);
 }
