5ce134644c13204095c797cddfa9c33848b3d502
Pablo Martin <pablomartincarrion@gmail.com>
2024-02-18 14:25:59 +0100

    finish only the first 3 exercises

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,8 @@
-function calculateArea() {
+function calculateArea(numberA, numberB) {
+
+    if(typeof numberA  !== "number" || typeof numberB !== "number"){
+        return undefined;
+    }
+      return numberA * numberB; 
 
 }
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,37 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("Should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
+
+        it("Should take two parameters", () => {
+            expect(calculateArea.length).toBe(2);
+        } )
+
+        it("Should return the division of the two numbers", () => {
+            expect( divide(1,2).toEqual(0.5));
+            expect( divide(3,4).toEqual(0.75));
+            expect( divide(10,10).toEqual(1));
+        })
+
+        it("Should return undefined if any of the parameters is not provided", () => {
+            expect(divide(1).toEqual(undefined));
+            except(divide().toEqual(undefined));
+            except( divide(undefined,1).toEqual(undefined));
+        })
+
+        it("Should return undefined if any of the parameters is not provided ", () => {
+            expect(divide(1,"2").toEqual(undefined));
+            except(divide("3",4).toEqual(undefined));
+            except( divide("100","47").toEqual(undefined));
+        })
 
     })    
 })
 
-*/
