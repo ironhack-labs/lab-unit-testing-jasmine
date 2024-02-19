66b35f2d23642ebcd601c010b15ac8d9620f5797
Mariana <marianabatalhadosloios@gmail.com>
2024-02-19 00:06:55 +0000

    lab done -bonus

--- src/add.js
+++ src/add.js
@@ -1,7 +1,7 @@
-function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
-        return undefined;
-    }
-
+function add(numOne, numTwo) {
+  if (typeof numOne === "number" && typeof numTwo === "number") {
     return numOne + numTwo;
+  } else {
+    return undefined;
+  }
 }
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,19 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+       it("should be defined", () => {
+         expect(calculateArea).toBeDefined();
+       });
 
+       it("should return the area of a rectangle when both arguments are provided", () => {
+         expect(calculateArea(6, 10)).toBe(60);
+         expect(calculateArea(4, 12)).toBe(48);
+       });
+
+       it("should return undefined if any of the arguments is not provided", () => {
+         expect(calculateArea(5)).toBeUndefined();
+         expect(calculateArea()).toBeUndefined();
+       });
     })    
 })
 
-*/
