efcb410a01bb6e45780d44135a8bdbb7ea300529
Raúl Dávila <raul.davila.es@gmail.com>
2024-02-25 16:00:07 +0100

    Solved lab

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,39 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
 
-describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+describe("Function - calculateArea", () => {
 
-    })    
-})
+    it("function should be defined", () => {
 
-*/
+        expect(calculateArea).toBeDefined()
+
+    });
+
+    it("the function should take two numbers as arguments", () => {
+
+        expected(calculateArea.length).toBe(2);
+    });
+
+    it("should return the rectangle area by multiplying the two numbers", () => {
+
+        expected(calculateArea(1, 2)).toEqual(2);
+        expect(calculateArea(3, 4)).toEqual(12);
+        expected(calculateArea(6, 6)).toEqual(36);
+
+    });
+
+    it("Should return undefined if any of the arfuments is not provided", () => {
+
+
+        expect(calculateArea(1)).toEqual(undefined);
+        expect(calculateArea()).toEqual(undefined);
+        expect(calculateArea(undefined, 1)).toEqual(undefined);
+    });
+
+    it("Should return undefined if any of the arguments is not a number", () => {
+
+        expect(calculateArea(1, " ")).toEqual(undefined);
+        expect(calculateArea(" ", 4)).toEqual(undefiend);
+        expect(calculateArea(" ", " ")).toEqual(undefined);
+    });
+});
--- tests/printFullName.spec.js
+++ tests/printFullName.spec.js
@@ -1,14 +1,31 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
 
 describe("Iteration 4", () => {
     describe("Function - printFullName", () => {
-        
+
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("the function should be defined", () => {
 
-    })    
+            expect(printFullName).toBeDefined()
+
+        });
+
+        it("should take one argument of type object", () => {
+
+
+            expect(printFullName.length).toBe(1);
+
+        });
+
+
+        it("should return a string with the first name and the last name separated by a space", () => {
+
+            expect(printFullName({ firstName: "Ed", lastName: "Coe" })).toEqual("Ed Coe");
+
+
+        });
+
+
+    })
 })
-
-*/
