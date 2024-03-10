8d9595e4ebd3033402803a10b30abe4e2afbe047
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-03-10 17:53:49 +0100

    Solved lab

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,9 @@
-function divide () {
-       
+function divide(a, b) {
+  let result = 0;
+  if (a == undefined || b == undefined) {
+    return undefined;
+  } else {
+    result = a / b;
+  }
+  return result;
 }
