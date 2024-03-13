0eb5ff698a523a813e1057091b5e2317e53d5bd6
John Doe <johndoe@example.com>
2024-03-13 18:32:44 +0100

    booksspec

--- src/add.js
+++ src/add.js
@@ -1,6 +1,19 @@
+// Answers to Iteration 1 | Getting Started:
+
+// 1. There are 4 tests for the add function in the add.spec.js file.
+// 2. The describe blocks are used to group related tests together, while the it blocks define individual test cases.
+//    The purpose of describe is to provide a higher-level organization, while it is used for specifying individual test cases.
+// 3. Test descriptions are phrased in a clear and concise manner, indicating what behavior is being tested. Keywords like "should" and "expected" stand out.
+// 4. The expect functions are used to define the expected behavior or outcome of the function being tested. They take the actual output of the function and compare it to the expected output.
+
+
 function add (numOne, numTwo) {
     if (numOne == undefined || numTwo == undefined) {
         return undefined;
+    }
+    if (typeof numOne != "number" || typeof nomTow != "number"){
+
+
     }
 
     return numOne + numTwo;
--- tests/add.spec.js
+++ tests/add.spec.js
@@ -3,22 +3,25 @@ describe("Iteration 1 | Getting Started", () => {
       it("should be defined", () => {
         expect(add).toBeDefined();
       });
-  
+
       it("should take two arguments", () => {
         expect(add.length).toBe(2);
       });
-  
+
       it("should return the sum of the two numbers", () => {
         expect(add(1, 2)).toEqual(3);
         expect(add(3, 4)).toEqual(7);
         expect(add(100, 47)).toEqual(147);
       });
-  
+
       it("should return undefined if any of the arguments is not provided", () => {
         expect(add(1)).toEqual(undefined);
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it ("should be undefined with different inputs and chack the result", number () => {
+        expect( add(1, "2") ).toEqual(undefined);
+        expect( add("3", 4) ).toEqual(undefined);
+        expect( add("100", "47") ).toEqual(undefined);
+      })
     });
-  });
-  
