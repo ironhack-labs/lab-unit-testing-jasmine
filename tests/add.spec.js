ab5078a23ba5fd4042a789d5b8be90a58bcbdb14
VinothaArivalagan <vinothag96@gmail.com>
2024-03-15 17:19:53 +0100

    unit testing

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,11 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+        
+        it("should return undefined if any of the arguments is not a number", () => {
+          expect(add(1, 2)).toEqual(undefined);
+          expect(add(5, 10)).toEqual(undefined);
+          expect(add(50, 1)).toEqual(undefined);
     });
   });
-  
+});
