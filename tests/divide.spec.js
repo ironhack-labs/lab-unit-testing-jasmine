c4afdbd1fa4a0616313ceea205d3e63655c431f3
Mirko Schaefer <codewithmirko@gmail.com>
2024-03-15 16:33:25 +0100

    corrected typo mirko schaefer

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -4,7 +4,7 @@ describe("Iteration 2", () => {
   describe("Function - divide", () => {
     // Each `it` block represents 1 test. You can use the following as a template:
     it("should be defined.", () => {
-      expect(add).toBeDefined();
+      expect(divide).toBeDefined();
     });
     it("should take two numbers as arguments.", () => {
       expect(divide.length).toBe(2);
