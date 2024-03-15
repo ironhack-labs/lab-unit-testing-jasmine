e04375aa9d6a4f5a92c94f9253324d53324dea3c
KemalGokten <ukemalgokten@gmail.com>
2024-03-15 13:36:09 +0100

    Finish add.js, and write test cases

--- src/add.js
+++ src/add.js
@@ -1,5 +1,5 @@
 function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
+    if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
         return undefined;
     }
 
