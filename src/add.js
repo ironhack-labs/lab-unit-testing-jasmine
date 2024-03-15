82a39e18833c33b51117847c4cbc4b19efe711a4
Rui Folgado <rui.folgado@hotmail.com>
2024-03-15 19:32:31 +0000

    solved lab, jasmin test

--- src/add.js
+++ src/add.js
@@ -1,7 +1,7 @@
 function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
+    
+    if (typeof numOne != "number" || typeof numTwo != "number"){
         return undefined;
     }
-
     return numOne + numTwo;
-}
+};
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,29 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+          });
+      
+          it("should take two arguments", () => {
+            expect(divide.length).toBe(2);
+          });
+      
+          it("should return the division of the two numbers", () => {
+            expect(divide(1, 2)).toEqual(0.5);
+            expect(divide(3, 4)).toEqual(0.75);
+            expect(divide(100, 50)).toEqual(2);
+          });
+      
+          it("should return undefined if any of the arguments is not provided", () => {
+            expect(divide(1)).toEqual(undefined);
+            expect(divide()).toEqual(undefined);
+            expect(divide(undefined, 1)).toEqual(undefined);
+          });
+    
+          it("should return undefined if any of the two arguments is not a number.", () => {
+            expect(divide(1, "2")).toEqual(undefined);
+            expect(divide("3", 4)).toEqual(undefined);
+            expect(divide("100", "47")).toEqual(undefined);
+          });
+        });
+     });
