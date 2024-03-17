f0e6b465229dc1e67a7196fdf01493d6f4ebd996
kingblocks <kivanckeskinbora@gmail.com>
2024-03-17 22:27:34 +0100

    Solved lab

--- src/add.js
+++ src/add.js
@@ -1,7 +1,9 @@
-function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
-        return undefined;
-    }
-
-    return numOne + numTwo;
+function add(numOne, numTwo) {
+  if (numOne == undefined || numTwo == undefined) {
+    return undefined
+  }
+  if (typeof numOne != 'number' || typeof numTwo != 'number') {
+    return undefined
+  }
+  return numOne + numTwo
 }
