6cac015c3547f5f4d2c7bbc1159944f0e657f25c
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2024-03-18 08:50:23 +0100

    Solved

--- src/add.js
+++ src/add.js
@@ -1,7 +1,7 @@
 function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
-        return undefined;
+    if (! (numOne == undefined || numTwo == undefined)) {
+        return numOne + numTwo;
     }
 
-    return numOne + numTwo;
+    
 }
