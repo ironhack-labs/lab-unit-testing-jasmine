b71abd48cb056c7ee62903d5828b926dfc7562aa
Michael Nguyen <michael.nguyen@live.nl>
2024-03-26 17:17:10 +0100

    iteration 1-3

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,7 @@
-function calculateArea() {
-
-}
+function calculateArea(x, y) {
+    if (typeof x !== "number" || typeof y !== "number") {
+      return undefined;
+    }
+  
+    return x * y;
+  }
